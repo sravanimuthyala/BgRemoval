5fa264d74f7e0ba62bbd876e0aab20c739f73d11
muthyalasravani-1 <sravani.m@trestlelabs.com>
2026-03-23 20:31:28 +0530

    Webhooks added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 //App config
 const PORT=process.env.PORT || 4000
@@ -12,4 +13,5 @@ app.use(express.json())
 app.use(cors())
 //API routes
 app.get('/',(req,res)=>res.send("API working"))
+app.use('/api/user',userRouter)
 app.listen(PORT,()=>console.log('server Running on port'+PORT))
