eafce583aedb86b64004e57af90396180d31c219
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-25 20:45:12 +0530

    Add lastName field to user model

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   photo: { type: String, required: true },
   firstname: { type: String },
-  creditbalance: { type: Number, default: 5 },
+  lastName:  { type: String},
+  creditbalance: { type: Number, default: 5 }
 });
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 export default userModel;
