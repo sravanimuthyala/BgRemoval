2cf3119b518774cd2b3dc611fbf6774ac0821ec5
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-25 20:20:30 +0530

    Add user webhook route to userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,1 +1,5 @@
-
+import express from 'express'
+import { clerkWebhooks } from '../controllers/UserController.js'
+const userRouter=express.Router()
+userRouter.post('/webhooks',clerkWebhooks)
+export default userRouter
