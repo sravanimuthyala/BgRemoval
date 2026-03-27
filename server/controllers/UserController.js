2cb26455a95af79ac46f66f716d181d2d2cc3024
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-27 07:03:11 +0530

    Implement userCredits function in UserController
    Added userCredits function to retrieve user credit balance.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -55,4 +55,19 @@ await whook.verify(JSON.stringify(req.body),{
   res.json({success:false,message:error.message})
  }
 }
-export {clerkWebhooks}
+
+//Api controller function to get user available credits data
+const userCredits=async(req,res)=>{
+try{
+  
+const {clerkId}=req.body
+const userData=await userModel.findOne({clerkId})
+res.json({success:true,credits:userData.creditbalance})
+}
+catch(error){
+  console.log(error.message)
+  res.json({success:false,message:error.message})
+ }
+}
+
+export {clerkWebhooks,userCredits}
