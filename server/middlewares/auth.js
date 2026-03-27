d2a39f91f80f273cc917ad7e6ef8e526049dd74a
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-27 09:16:43 +0530

    Comment out authUser middleware code

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,18 +1,18 @@
-import jwt from "jsonwebtoken";
-const authUser = async (req, res, next) => {
-  try {
-    const {token}=req.headers
+// import jwt from "jsonwebtoken";
+// const authUser = async (req, res, next) => {
+//   try {
+//     const {token}=req.headers
 
-    if (!token) {
-        return res.json({success:false,message:'Not Authorized Login Again'})
-    }
+//     if (!token) {
+//         return res.json({success:false,message:'Not Authorized Login Again'})
+//     }
 
-    const token_decode=jwt.decode(token);
-    req.body.clerkId=token_decode.clerkId;
-    next()
-  } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
-  }
-};
-export default authUser
+//     const token_decode=jwt.decode(token);
+//     req.body.clerkId=token_decode.clerkId;
+//     next()
+//   } catch (error) {
+//     console.log(error.message);
+//     res.json({ success: false, message: error.message });
+//   }
+// };
+// export default authUser
