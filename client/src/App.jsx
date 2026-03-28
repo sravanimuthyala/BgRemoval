707b4d867cc9dc54bb423e45001425101112c488
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-28 20:03:34 +0530

    Add ToastContainer and update route path for BuyCredit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,16 @@ import BuyCredit from "./pages/BuyCredit";
 import Result from "./pages/Result";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
+  import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
   return (
     <div className="min-h-screen bg-slate-50">
+      <ToastContainer position='bottom-right'/>
       <Navbar />
       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/buy-credit" element={<BuyCredit />} />
+        <Route path="/buy" element={<BuyCredit />} />
         <Route path="/result" element={<Result />} />
       </Routes>
       <Footer/>
