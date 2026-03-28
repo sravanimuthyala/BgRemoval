29ee4ac6febdde619cf9046352c599c499a77056
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-28 20:09:14 +0530

    Create multer.js

---
+++ server/middlewares/multer.js
@@ -0,0 +1,9 @@
+import multer from "multer";
+const storage=multer.diskStorage({
+    filename:function(re,file,callback){
+        callback(null,`${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload=multer({storage})
+export default upload
