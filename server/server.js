077f8189df7e422819d1764c34707e837cc8b77e
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-28 20:12:29 +0530

    Add image routes to server configuration

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 //App config
 const PORT=process.env.PORT || 4000
@@ -14,4 +15,5 @@ app.use(cors())
 //API routes
 app.get('/',(req,res)=>res.send("API working"))
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 app.listen(PORT,()=>console.log('server Running on port'+PORT))
