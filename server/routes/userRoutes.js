70ff1b66a2791f051fc5acaa4c5b887741e847b0
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-28 20:11:41 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,8 @@
 import express from 'express'
-import { clerkWebhooks } from '../controllers/UserController.js'
+import { clerkWebhooks, paymentRazorpay, userCredits } from '../controllers/UserController.js'
+import authUser from '../middlewares/auth.js'
 const userRouter=express.Router()
 userRouter.post('/webhooks',clerkWebhooks)
+userRouter.get('/credits',authUser,userCredits)
+userRouter.post('/pay-razor',authUser,paymentRazorpay)
 export default userRouter
