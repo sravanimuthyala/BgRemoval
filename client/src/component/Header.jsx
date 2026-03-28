c455cac35258476691ac7228431b0ea312d2b91e
sravanimuthyala <sravanimuthyala19@gmail.com>
2026-03-28 19:59:08 +0530

    Integrate removeBg function from AppContext in Header
    Added context usage for removeBg function in Header component.

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Header = () => {
+  const{removeBg}=useContext(AppContext)
   return (
     <div className="flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20">
       {/* Left Side */}
@@ -22,7 +25,7 @@ const Header = () => {
           Lorem Ipsum has been the industry's standard dummy text ever.
         </p>
         <div>
-          <input type="file" name="" id="upload1" hidden />
+          <input onChange={e=>removeBg(e.target.files[0])} type="file" accept="image/*" id="upload1" hidden />
           <label
             className="inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700 "
             htmlFor="upload1"
